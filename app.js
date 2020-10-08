58062fcc50bee582558c64ad7870fc41573a1ab5
jeetintyagi <jt9999709701@gmail.com>
2020-10-08 13:26:29 +0530

    CRUD operations,CSS & detail page done

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { render } = require("ejs");
 const express = require("express");
 const app = express();
 
@@ -24,80 +25,81 @@ mongoose
     console.log(err);
   });
 
-//register for view engine
+// register view engine
 app.set("view engine", "ejs");
 
-//middlewares and static files
+// middleware & static files
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-
-//mongoose and mongo sandbox routes
-app.get("/create-blog", (req, res) => {
-  const blog = new Blog({
-    title: "Lorem ipsum dolor sit amet",
-    snippet: "about the blog",
-    body:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus quidem harum rerum possimus sed nulla, minus eaque veniam earum error dolores eius iure iste repellat! Quasi quia omnis nam corrupti. ",
-  });
-
-  blog
-    .save()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
 });
 
-app.get("/myblogs", (req, res) => {
-  Blog.find()
-    .then((result) => {
-      const list = JSON.stringify(result);
-      res.send(list);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/searchblog", (req, res) => {
-  Blog.findById("5f759c022efe642a940440eb")
-    .then((result) => {
-      const list = JSON.stringify(result);
-      res.send(list);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-//!ROUTES
+// routes
 app.get("/", (req, res) => {
   res.redirect("/blogs");
 });
 
 app.get("/about", (req, res) => {
-  res.status(200).render("about", { title: "About" });
+  res.render("about", { title: "About" });
 });
 
-// blogs routes
+// blog routes
 app.get("/blogs/create", (req, res) => {
-  res.status(200).render("create", { title: "Create blog" });
+  res.render("create", { title: "Create a new blog" });
 });
 
 app.get("/blogs", (req, res) => {
   Blog.find()
     .sort({ createdAt: -1 })
     .then((result) => {
-      res.render("index", { title: "My blogs", blogs: result });
+      res.render("index", { blogs: result, title: "All blogs" });
     })
     .catch((err) => {
       console.log(err);
     });
 });
 
-//Error page rendering
+app.post("/blogs", (req, res) => {
+  // console.log(req.body);
+  const blog = new Blog(req.body);
+
+  blog
+    .save()
+    .then((result) => {
+      res.redirect("/blogs");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((result) => {
+      res.render("details", { blog: result, title: "Blog Details" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.delete("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+
+  Blog.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: "/blogs" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+// 404 page
 app.use((req, res) => {
-  res.status(404).render("404", { title: "Not found" });
+  res.status(404).render("404", { title: "404" });
 });
--- models/blog.js
+++ models/blog.js
@@ -20,6 +20,5 @@ const blogSchema = new Schema({
 
 },{timestamps:true});
 
-
 const Blog = mongoose.model('Blog', blogSchema);
 module.exports = Blog;
